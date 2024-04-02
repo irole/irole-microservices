5c91f2f5ca41ebcfec3de361b0b98e1600970ed7
samb2 <samb21272@gmail.com>
2024-04-02 16:18:03 +0330

    v1.13.0 pattern.enum update!

--- src/enums/pattern.enum.ts
+++ src/enums/pattern.enum.ts
@@ -1,6 +1,7 @@
 export enum PatternEnum {
   USER_REGISTERED = 'user_registered',
   AUTH_VERIFY_TOKEN = 'auth_verify_token',
+  AUTH_UPDATE_USER = 'auth_update_user',
   USER_AVATAR_UPLOADED = 'user_avatar_uploaded',
   USER_AVATAR_DELETED = 'user_avatar_deleted',
 }
